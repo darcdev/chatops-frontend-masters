fc5c3ad43d0ef5190f5bae5edb0726d09de63a74
darcdev <diegoandresrojas2000@gmail.com>
2024-09-07 11:50:12 -0500

    feat: working chatops food fight flow, with commands, shortcuts, blocks kit, and interoperating with notion

--- src/slack.ts
+++ src/slack.ts
@@ -1,21 +1,47 @@
 import type { Handler } from '@netlify/functions';
 import {parse} from 'querystring';
-import {slackApi, verifySlackRequest} from "./util/slack";
+import {slackApi, verifySlackRequest, modal, blocks, handleInteractivity} from "./util/slack";
 
 export async function handleSlashCommand(payload : SlackSlashCommandPayload){
-	console.log(payload.command)
 	switch (payload.command){
 		case '/foodfight' :
-			const response = await slackApi("chat.postMessage", {
-				channel : payload.channel_id,
-				text : 'Italian Food is delicious!!!'
-			})
+			const response = await slackApi(
+				'views.open',
+				modal({
+					id: 'foodfight-modal',
+					title: 'Start a food fight!',
+					trigger_id: payload.trigger_id,
+					blocks: [
+						blocks.section({
+							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
+						}),
+						blocks.input({
+							id: 'opinion',
+							label: 'Deposit your controversial food opinions here.',
+							placeholder:
+								'Example: peanut butter and mayonnaise sandwiches are delicious!',
+							initial_value: payload.text ?? '',
+							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
+						}),
+						blocks.select({
+							id: 'spice_level',
+							label: 'How spicy is this opinion?',
+							placeholder: 'Select a spice level',
+							options: [
+								{ label: 'mild', value: 'mild' },
+								{ label: 'medium', value: 'medium' },
+								{ label: 'spicy', value: 'spicy' },
+								{ label: 'nuclear', value: 'nuclear' },
+							],
+						}),
+					],
+				}),
+			);
+
 			if(!response.ok){
-				return {
-					statusCode : 500,
-					body : 'No se puedo enviar el mensaje al chat' // lo que el que envia el mensaje ve
-				}
+				console.log(response)
 			}
+
 			return {
 				statusCode : 200,
 				body : '' // lo que el que envia el mensaje ve
@@ -48,6 +74,11 @@ export const handler: Handler = async (event) => {
 	}
 
 	// TODO handle interactivity (e.g. context commands, modals)
+
+	if(body.payload){
+		const payload = JSON.parse(body.payload)
+		return handleInteractivity(payload);
+	}
 
 	return {
 		statusCode: 200,
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,5 +1,6 @@
 import type {HandlerEvent} from "@netlify/functions";
 import {createHmac} from "node:crypto";
+import {saveItem} from "./notion";
 
 // TODO create Slack utilities
 
@@ -18,7 +19,7 @@ export function slackApi(
 }
 
 export function verifySlackRequest(request : HandlerEvent){
-    const secret = process.env.SLACK_SIGNING_SECRET;
+    const secret = process.env.SLACK_SIGNING_SECRET as string;
     const signature = request.headers['x-slack-signature'];
     const timestamp = Number(request.headers['x-slack-request-timestamp']);
     const now = Math.floor(Date.now() / 1000)
@@ -33,79 +34,147 @@ export function verifySlackRequest(request : HandlerEvent){
 }
 
 export const blocks = {
-    section : ({text} : SectionBlockArgs): SlackBlockSection => {
+    section: ({ text }: SectionBlockArgs): SlackBlockSection => {
         return {
-            type : 'section',
-            text : {
-                type:  'mrkdwn',
-                text
-            }
-        }
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text,
+            },
+        };
     },
-    input: ({id, label, placeholder, initial_value='', hint = ''} : InputBlockArgs) : {
-        hint: { text: string | undefined; type: string };
-        label: { text: string; type: string };
-        type: string;
-        block_id: string;
-        element: {
-            initial_value: string | undefined;
-            action_id: string;
-            placeholder: { text: string; type: string };
-            type: string
-        }
-    } => {
+    input({
+              id,
+              label,
+              placeholder,
+              initial_value = '',
+              hint = '',
+          }: InputBlockArgs): SlackBlockInput {
         return {
             block_id: `${id}_block`,
             type: 'input',
             label: {
                 type: 'plain_text',
-                text: label
+                text: label,
             },
             element: {
                 action_id: id,
                 type: 'plain_text_input',
                 placeholder: {
                     type: 'plain_text',
-                    text: placeholder
+                    text: placeholder,
                 },
-                initial_value
+                initial_value,
             },
-            hint : {
-                type : 'plain_text',
-                text : hint
-            }
-        }
+            hint: {
+                type: 'plain_text',
+                text: hint,
+            },
+        };
     },
-    select : ({
-                  id, label, placeholder, options
-              } : SelectBlockArgs) : SlackBlockInput => {
+    select({
+               id,
+               label,
+               placeholder,
+               options,
+           }: SelectBlockArgs): SlackBlockInput {
         return {
-            block_id : `${id}_block`,
-            type : 'input',
+            block_id: `${id}_block`,
+            type: 'input',
             label: {
                 type: 'plain_text',
-                text: label
+                text: label,
+                emoji: true,
             },
-            element : {
-                action_id : id,
-                type : 'static_select',
-                placeholder : {
-                    type : 'plain_text',
-                    text : placeholder,
-                    emoji : true
+            element: {
+                action_id: id,
+                type: 'static_select',
+                placeholder: {
+                    type: 'plain_text',
+                    text: placeholder,
+                    emoji: true,
                 },
-                options : options.map(({label, value}) => {
+                options: options.map(({ label, value }) => {
                     return {
-                        text : {
-                            type : 'plain_text',
+                        text: {
+                            type: 'plain_text',
                             text: label,
-                            emoji : true
+                            emoji: true,
                         },
-                        value : value
-                    }
-                })
+                        value,
+                    };
+                }),
             },
-        }
-    }
+        };
+    },
+};
 
+export function modal({
+                          trigger_id,
+                          id,
+                          title,
+                          submit_text = 'Submit',
+                          blocks,
+                      }: ModalArgs) {
+    return {
+        trigger_id,
+        view: {
+            type: 'modal',
+            callback_id: id,
+            title: {
+                type: 'plain_text',
+                text: title,
+            },
+            submit: {
+                type: 'plain_text',
+                text: submit_text,
+            },
+            blocks,
+        },
+    };
+}
+
+export async function handleInteractivity(payload : SlackModalPayload){
+    const callback_id = payload.callback_id ?? payload.view.callback_id
+    switch (callback_id){
+        case 'foodfight-modal' :
+            const data =  payload.view.state.values
+            const fields = {
+                opinion : data.opinion_block.opinion.value,
+                spiceLevel : data.spice_level_block.spice_level.selected_option.value,
+                submitter : payload.user.name,
+            }
+
+            await saveItem(fields)
+
+            await slackApi('chat.postMessage', {
+                channel : 'C07LEH3E9B6',
+                text : `Oh dang, y'all! :eyes: <@${payload.user.id}> just started a food figth with a ${fields.spiceLevel} take:\n\n ${fields.opinion}\n\n...discuss`
+            })
+            console.log('todo bien')
+            break;
+        case 'start-food-fight-nudge':
+            const channel = payload.channel?.id;
+            const user_id = payload.user.id;
+            const thread_ts = payload.message.thread_ts ?? payload.message.ts;
+
+            await slackApi('chat.postMessage', {
+                channel,
+                thread_ts,
+                text: `Hey <@${user_id}>, an opinion like this one deserves a heated public debate. Run the \`/foodfight\` slash command in a main channel to start one!`,
+            });
+
+            break;
+        default:
+            console.log(`not handler defined for ${callback_id}`)
+            return {
+                statusCode : 400,
+                body : `not handler defined for ${callback_id}`
+            }
+    }
+    console.log('todo bien 2')
+    return {
+        statusCode : 200,
+        body : ''
+    }
 }
